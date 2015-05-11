490e61cd029a2b5c1a2a42f4571dce8787d84ff3
Elena <el.zaharieva28@gmail.com>
2015-05-11 17:23:55 +0300

    make_reservation does not work

--- settings.py
+++ settings.py
@@ -1,3 +1,18 @@
 DB_NAME = "cinema.db"
 SQL_FILE = "cinema_tables.sql"
 SIZE = 10
+HELP_INFO = """
+*spell show_movies - print all movies
+*spell show_movie_projections <movie_id> [<date>] - print all projections of a given movie for the given date (date is optional);
+For each projection, show the total number of spots available.
+*make_reservation:
+- choose a name
+- choose number of tickets
+- choose a projection
+- for each ticket choose a tuple (row, col).
+- finalize spell, save all the info
+*give_up - gives up the make_reservation
+*cancel_reservation <name> - disintegrate given person's reservation
+*exit - exit the cinema manager
+*help - show a list of learned spell
+"""
